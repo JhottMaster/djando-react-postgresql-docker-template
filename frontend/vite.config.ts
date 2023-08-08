478a0d1a9abd9fd03562685ea2cc8b8d4d619b0e
Pablo <pablo.aizpiri@gmail.com>
2023-08-07 23:10:39 -0500

    Containarize services

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // We have to listen on 0.0.0.0 (all addresses) in order to let docker discover this service:
+    host: '0.0.0.0'
+  },
   plugins: [react()],
 })
